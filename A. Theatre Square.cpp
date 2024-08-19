382f6979fb0ea90a6692f657279bac21ce4fbb0e
Tomasz Dubiel <dubieltomasz420@gmail.com>
2024-08-19 22:34:51 +0200

    Add files via upload

---
+++ A. Theatre Square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  unsigned long long n, m, a;
+  std::string answer = "";
+
+  std::cin >> n >> m >> a;
+  
+  n = std::ceil((double)n/a);
+  m = std::ceil((double)m/a);
+
+  std::cout << n * m;
+}
